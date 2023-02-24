a237a8fb1f03873b432ecb3e0b84515e19889f89
jongyonghwa <ttl33787@gmail.com>
2023-02-24 17:48:13 +0900

    230224예외처리,기본 api

--- 01_Java/a009_exception/src/edu/kh/exception/run/ExceptionRun.java
+++ 01_Java/a009_exception/src/edu/kh/exception/run/ExceptionRun.java
@@ -3,14 +3,15 @@ package edu.kh.exception.run;
 import java.io.IOException;
 
 import edu.kh.exception.service.ExceptionService;
+import edu.kh.exception.user.exception.ScoreInputException;
 
 public class ExceptionRun {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ScoreInputException {
 
 		ExceptionService service = new ExceptionService();
 		
-		service.ex3();
+		service.startEx5();
 	}
 
 }
