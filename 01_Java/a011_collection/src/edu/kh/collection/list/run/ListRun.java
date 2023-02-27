d15588954baf837de4b4e7912dfac77fd6d2dd1f
jongyonghwa <ttl33787@gmail.com>
2023-02-27 17:43:58 +0900

    230227 컬렉션

---
+++ 01_Java/a011_collection/src/edu/kh/collection/list/run/ListRun.java
@@ -0,0 +1,14 @@
+package edu.kh.collection.list.run;
+
+import edu.kh.collection.list.service.ListService;
+
+public class ListRun {
+
+		public static void main(String[] args) {
+			
+			ListService ls = new ListService();
+			
+//			ls.ex1();
+			ls.ex2();
+		}
+}
