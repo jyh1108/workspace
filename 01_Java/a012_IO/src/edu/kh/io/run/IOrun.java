4cfc8785ca8641bcce88d94b6a90dfbd7d545fd6
jongyonghwa <ttl33787@gmail.com>
2023-03-03 17:46:03 +0900

    230303 프로젝트 게임

---
+++ 01_Java/a012_IO/src/edu/kh/io/run/IOrun.java
@@ -0,0 +1,22 @@
+package edu.kh.io.run;
+
+import edu.kh.io.service.IOService;
+
+public class IOrun {
+
+	public static void main(String[] args) {
+		
+		IOService service = new IOService();
+		
+//		service.byteOutPut();
+//		service.charOutPut();
+//		service.byteInput();
+//		service.charInput();
+//		service.fileCopy();
+//		service.objectOutput();
+//		service.objectInput();
+//		service.listOutput();
+		service.listInput();
+	}
+
+}
