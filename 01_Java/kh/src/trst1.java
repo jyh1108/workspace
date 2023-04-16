890e843b61dd58eeeb7bfa30c5ac19a4cffdfcc3
java <ttl33787@gmail.com>
2023-04-17 00:00:44 +0900

    코테

--- 01_Java/kh/src/trst1.java
+++ 01_Java/kh/src/trst1.java
@@ -1,26 +1,53 @@
-import java.util.Arrays;
 
 public class trst1 {
 
-public static void main(String[] args) {
-		int [] [] number =	{{80, 70}, {90, 50}, {40, 70}, {50, 80}};
-		int [] svg = new int[number.length];
-		int[] answer = new int[svg.length];
-		int a =0;
-		
-		for(int i =0; i<number.length; i++) {
-			svg[i] = ((number[i][0]+number[i][1])/2);
-		}
-		for(int j=0; j<svg.length; j++) {
-			for(int y =0; y<j; y++) {
-				if(a<svg[y]) {
-					a=svg[y];
+	public static void main(String[] args) {
+		String[] dic = { "moos", "dzx", "smm", "sunmmo", "som" };
+		String[] spell = { "s", "o", "m", "d" };
+		int answer = 2;
+
+		for (int i = 0; i < dic.length; i++) {
+			int count = 0;
+			int err =0;
+			for (int j = 0; j < spell.length; j++) {
+				if (dic[i].contains(spell[j])) {
+					count++;
+				}else{
+					err++;
 				}
-			}answer[i]
+			}
+			if (count == dic[i].length()&&err==0) {
+				answer = 1;
+			}
 		}
-		System.out.println(Arrays.toString(svg));
-		System.out.println(Arrays.toString(answer));
-
+		System.out.println(answer);
+//		 int answer = 2;
+//			ArrayList<Character>[] ArrayL1 = new ArrayList[dic.length];
+//			for (int i = 0; i < dic.length; i++) {
+//				ArrayL1[i] = new ArrayList<>();
+//				for (int j = 0; j < dic[i].length(); j++) {
+//					ArrayL1[i].add(dic[i].charAt(j));
+//				}
+//			}
+//
+//			for (int k = 0; k < ArrayL1.length; k++) {
+//				int yes = 0;
+//				for (int u = 0; u < ArrayL1[k].size(); u++) {
+//					for (int s = 0; s < spell.length; s++) {
+////						System.out.print(svg[s]);
+////						System.out.println(ArrayL1[k].get(u).charValue());
+//						if (spell[s] == (ArrayL1[k].get(u).charValue())) {
+//							yes++;
+////							System.out.println("더하기1");
+//						}
+//					}
+//				}
+//				if (yes == ArrayL1[k].size()) {
+//					answer = 1;
+//					break;
+//				} else {
+////					System.out.println(yes);
+//				}
+//			}
 	}
-
 }
