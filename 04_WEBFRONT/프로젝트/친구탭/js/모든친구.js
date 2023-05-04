0fd51fd652ae8e2926901e6c2435cad36fa5d0a4
jeongyonghwa <ttl33787@gmai.com>
2023-05-04 14:32:39 +0900

    230504 project

--- 04_WEBFRONT/프로젝트/친구탭/js/모든친구.js
+++ 04_WEBFRONT/프로젝트/친구탭/js/모든친구.js
@@ -5,32 +5,40 @@ const frmenu = document.querySelectorAll(".frmenu");
 
 for (let i = 0; i < frtopdiv.length; i++) {
 
-frtopdiv[i].onmouseover = function (){
-  pulsnav[i].style.backgroundColor ="#EBEDF0";
-}
-frtopdiv[i].onmouseout = function(){
-  pulsnav[i].style.backgroundColor ="#ffffff";
-}
-// pulsnav[i].addEventListener("click", e => {
-//   if(frmenu[i].style.display != "none"){
-//     frmenu[i].style.display = "none";
-//     console.log("test");
-//   }else{
-//     frmenu[i].style.display = "block";
-//     console.log("test1");
-//   }
-// })
-
-
-const outclick = e => {
-  if (!frmenu[i].contains(e.target)) { // 클릭한 요소가 frmenu의 하위 요소인지 확인
-    frmenu[i].style.display = "none"; // frmenu를 숨김
-    document.removeEventListener("click", outclick); // 클릭 이벤트 리스너 제거
+  frtopdiv[i].onmouseover = function () {
+    pulsnav[i].style.backgroundColor = "#EBEDF0";
   }
-};
-pulsnav[i].addEventListener("click", e => {
-  e.stopPropagation(); // pulsnav 클릭 이벤트 전파 방지
-  frmenu[i].style.display = "block"; // frmenu를 보임
-  document.addEventListener("click", outclick); // 클릭 이벤트 리스너 등록
-});
+  frtopdiv[i].onmouseout = function () {
+    pulsnav[i].style.backgroundColor = "#ffffff";
+  }
+  // pulsnav[i].addEventListener("click", e => {
+  //   if(frmenu[i].style.display != "none"){
+  //     frmenu[i].style.display = "none";
+  //     console.log("test");
+  //   }
+  // })
+
+
+  const outclick = e => {
+    if (!frmenu[i].contains(e.target)) { // 클릭한 요소가 frmenu의 하위 요소인지 확인
+      frmenu[i].style.display = "none"; // frmenu를 숨김
+      document.removeEventListener("click", outclick); // 클릭 이벤트 리스너 제거
+    }
+  };
+
+  
+  pulsnav[i].addEventListener("click", e => {
+    if (frmenu[i].style.display == "block") {
+      for (let i = 0; i < frtopdiv.length; i++) {
+        frmenu[i].style.display = "none";
+      }
+    } else {
+      for (let i = 0; i < frtopdiv.length; i++) {
+        frmenu[i].style.display = "none";
+      }
+      e.stopPropagation(); // pulsnav 클릭 이벤트 전파 방지
+      frmenu[i].style.display = "block"; // frmenu를 보임
+      document.addEventListener("click", outclick); // 클릭 이벤트 리스너 등록
+    }
+  });
 };
